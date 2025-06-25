a847cc9be307b7ecbb6072a3a84c25b4d2a0b21f
LucasSandroRotermelFranco <lucassandrorotermel.franco@philips.com>
2025-06-25 14:52:37 -0300

    feat:Adding project

---
+++ app/src/main/java/org/example/TempConverter.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class TempConverter {
+    private double temperatura;
+    private String tipo;
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double calcular() {
+        if (tipo.equals("C")) {
+            return (temperatura - 32) * 0.55; // Fahrenheit to Celsius
+        } else if (tipo.equals("F")) {
+            return (temperatura * 1.8) + 32; // Celsius to Fahrenheit
+        } else {
+            throw new IllegalArgumentException("Tipo inválido. Use 'C' para Celsius ou 'F' para Fahrenheit.");
+        }
+    }
+}
